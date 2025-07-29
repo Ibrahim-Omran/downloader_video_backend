aa13ab134b2ec16d619c5daaa50427f4f70a87f3
Ibrahim-Omran <112516011+Ibrahim-Omran@users.noreply.github.com>
2025-07-29 23:33:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def get_video_url():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
